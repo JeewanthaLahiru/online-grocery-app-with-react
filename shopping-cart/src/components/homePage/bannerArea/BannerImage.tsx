6930c1a3e7e39563abb6e88c5e7b1cd223960490
JeewanthaLahiru <jeewanthalahiru00@gmail.com>
2021-03-02 00:51:29 +0530

    added banner image

--- shopping-cart/src/components/homePage/bannerArea/BannerImage.tsx
+++ shopping-cart/src/components/homePage/bannerArea/BannerImage.tsx
@@ -5,7 +5,7 @@ import banner_image from "../../../assets/images/banner_images/banner.jpg";
 const BannerImage:React.FC = () => {
     return (
         <Container fluid={true} className="banner-image m-0 p-0" >
-            <h1>banner</h1>
+            <img src={banner_image} alt="banner image" />
         </Container>
     )
 }
