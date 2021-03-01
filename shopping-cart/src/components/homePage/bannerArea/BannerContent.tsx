a647a9264803463b37284b3f942796884f094dc0
JeewanthaLahiru <jeewanthalahiru00@gmail.com>
2021-03-02 00:26:34 +0530

    created bannerImage and bannerItem components

---
+++ shopping-cart/src/components/homePage/bannerArea/BannerContent.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const BannerContent:React.FC = () => {
+    return (
+        <div>Banner content</div>
+    )
+}
+
+export default BannerContent;
---
+++ shopping-cart/src/components/homePage/bannerArea/BannerImage.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const BannerImage:React.FC = () => {
+    return (
+        <div>banner image</div>
+    )
+}
+
+export default BannerImage;
