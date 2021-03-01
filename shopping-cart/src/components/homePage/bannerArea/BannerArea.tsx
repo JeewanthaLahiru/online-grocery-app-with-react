4d60284977fc456d00c12078ab408a02da9f1bf0
JeewanthaLahiru <jeewanthalahiru00@gmail.com>
2021-03-02 00:29:53 +0530

    created banner image component

--- shopping-cart/src/components/homePage/bannerArea/BannerArea.tsx
+++ shopping-cart/src/components/homePage/bannerArea/BannerArea.tsx
@@ -1,8 +1,27 @@
 import React from 'react';
+import {Carousel, Container} from "react-bootstrap";
+import BannerImage from "./BannerImage";
+import BannerContent from "./BannerContent";
 
 const BannerArea:React.FC = () => {
     return (
-        <div>Banner Area</div>
+        <Container fluid={true} className="banner-area m-0 p-0" >
+            <Carousel>
+                <Carousel.Item interval={4000}>
+                    <BannerImage/>
+                    <BannerContent/>
+                </Carousel.Item>
+                <Carousel.Item interval={4000}>
+                    <BannerImage/>
+                    <BannerContent/>
+                </Carousel.Item>
+                <Carousel.Item interval={4000}>
+                    <BannerImage/>
+                    <BannerContent/>
+                </Carousel.Item>
+            </Carousel>
+
+        </Container>
     )
 }
 
--- shopping-cart/src/components/homePage/bannerArea/BannerImage.tsx
+++ shopping-cart/src/components/homePage/bannerArea/BannerImage.tsx
@@ -1,8 +1,12 @@
 import React from "react";
+import {Container} from "react-bootstrap";
+import banner_image from "../../../assets/images/banner_images/banner.jpg";
 
 const BannerImage:React.FC = () => {
     return (
-        <div>banner image</div>
+        <Container fluid={true} className="banner-image m-0 p-0" >
+            <img src={banner_image} alt="banner image" />
+        </Container>
     )
 }
 
