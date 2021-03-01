fd4a864ba84145f628197ba8d31692322b928575
JeewanthaLahiru <jeewanthalahiru00@gmail.com>
2021-03-02 00:52:03 +0530

    added banner content

--- shopping-cart/src/components/homePage/bannerArea/BannerContent.tsx
+++ shopping-cart/src/components/homePage/bannerArea/BannerContent.tsx
@@ -5,7 +5,15 @@ const BannerContent:React.FC = () => {
     return (
         <Container fluid={true} className="banner-content m-0 p-0">
             <Row  className={"p-0 m-0"}>
-                <h1>banner content</h1>
+                <Col xs={{span:9, offset:3}} sm={{span:6, offset:6}} md={{span:5, offset:7}}>
+                    <Container fluid={true} className="logo-container m-0 p-0" >
+                        <img src={leaves_logo} alt="leaves logo" />
+                    </Container>
+                    <h4 className="text-secondary">100% Healthy and Affordable</h4>
+                    <h1 className="text-success" >Organic Vegetables</h1>
+                    <h4 className="text-secondary">Small Chages Big Difference</h4>
+                    <Button variant="light" >Shop Now</Button>
+                </Col>
             </Row>
         </Container>
     )
