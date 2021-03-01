3ebd5efad1b1a0d43b95cf22314fe655b999f2dd
JeewanthaLahiru <jeewanthalahiru00@gmail.com>
2021-03-01 22:58:53 +0530

    added bottom header navigation

--- shopping-cart/src/components/homePage/headerArea/BottomHeader.tsx
+++ shopping-cart/src/components/homePage/headerArea/BottomHeader.tsx
@@ -1,8 +1,27 @@
 import React from "react";
+import {Button, Container, Nav, Navbar} from "react-bootstrap";
 
 const BottomHeader:React.FC = () => {
     return (
-        <div>BottomHeader</div>
+        <Container fluid={true} className="m-0 p-0 bottom-header" >
+            <div className="shadow-line" ></div>
+            <Navbar bg="white" expand="lg" >
+                <Navbar.Brand href="#home" className="text-success bottom-header-title" >
+                    <span>LOGO</span>
+                </Navbar.Brand>
+
+                <Nav className="ml-auto">
+                    <Nav.Link href="#home" className="notification">
+                        <span><i className="feather icon-shopping-cart"/></span>
+                        <span className="badge">2</span>
+                    </Nav.Link>
+
+                </Nav>
+                <Nav.Link href="#link">
+                    <Button variant="success" size="lg" className="check-out-button" >Check out</Button>
+                </Nav.Link>
+            </Navbar>
+        </Container>
     )
 }
 
