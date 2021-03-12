b6c251f84b9689abcbe71c91ba98aa852017ef0c
JeewanthaLahiru <jeewanthalahiru00@gmail.com>
2021-03-13 00:55:05 +0530

    close the lable with state

--- shopping-cart/src/components/homePage/categoryListArea/CategoryListArea.tsx
+++ shopping-cart/src/components/homePage/categoryListArea/CategoryListArea.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 import {Col, Row} from "react-bootstrap";
 import CategoryItem from "./CategoryItem";
 import AllCat from "../../../assets/images/product_category/allcategories.jpg";
@@ -8,16 +8,25 @@ import Food from "../../../assets/images/product_category/food.png";
 import Electro from "../../../assets/images/product_category/electro.png";
 
 const ProductListArea:React.FC = () => {
+
+    const [isLabelVisible, setIsLabelVisible] = useState(true);
+
+    const closeLabel = () => {
+        setIsLabelVisible(false);
+    }
+
     return(
         <Row className="mx-0 px-lg-5 category-list-area">
-            <Col className="m-0 px-5">
-                <Row className=" closing-label" >
+            <Col className="m-0 px-lg-5">
+                { isLabelVisible && <Row className=" closing-label my-2" >
                     <Col className="closing-label-inside" >
-                        <p>*Products listed here are demo purpose
-                        only. They are not available for sale.</p>
-                        <i className="feather icon-x" ></i>
+                        <Row>
+                            <Col xs={10}><p>*Products listed here are demo purpose
+                                only. They are not available for sale.</p></Col>
+                            <Col xs={2}><i className="feather icon-x" onClick={()=> closeLabel()} ></i></Col>
+                        </Row>
                     </Col>
-                </Row>
+                </Row>}
                 <Row className="m-0 p-0">
                     <Col xs={12} className="m-0 p-0">
                         <h2 className="my-3 text-success text-center p-0">Demo Products</h2>
