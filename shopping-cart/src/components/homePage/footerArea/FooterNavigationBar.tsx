fe41b3260d68f2194f365e0f99c932b8c4617f08
JeewanthaLahiru <jeewanthalahiru00@gmail.com>
2021-03-13 23:15:27 +0530

    added footer navigationbar links

--- shopping-cart/src/components/homePage/footerArea/FooterNavigationBar.tsx
+++ shopping-cart/src/components/homePage/footerArea/FooterNavigationBar.tsx
@@ -5,9 +5,10 @@ const FooterNavigationBar:React.FC = () => {
     return(
         <Navbar variant="dark" className={'mt-3 py-0 px-lg-8 footer-background footer-label2-color text-center'}>
             <Nav className="m-auto nav-section">
+                <Nav.Link className={'mx-md-2'}>HOME</Nav.Link>
                 <Nav.Link className={'mx-md-2'}>ABOUT US</Nav.Link>
                 <Nav.Link className={'mx-md-2'}>FAQ</Nav.Link>
-                <Nav.Link className={'mx-md-2'}>DELIVERY INFO</Nav.Link>
+                <Nav.Link className={'mx-md-2'}>PRICING</Nav.Link>
                 <Nav.Link className={'mx-md-2'}>CONTACT US</Nav.Link>
             </Nav>
         </Navbar>
