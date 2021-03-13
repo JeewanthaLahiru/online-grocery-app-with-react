13829a55030b3bf84c1f87d662843648a1e5b2e0
JeewanthaLahiru <jeewanthalahiru00@gmail.com>
2021-03-14 00:54:07 +0530

    created cart items component

--- shopping-cart/src/components/homePage/headerArea/BottomHeader.tsx
+++ shopping-cart/src/components/homePage/headerArea/BottomHeader.tsx
@@ -1,27 +1,36 @@
-import React from "react";
+import React, {useState} from "react";
 import {Button, Container, Nav, Navbar} from "react-bootstrap";
+import CartArea from "./CartArea";
 
 const BottomHeader:React.FC = () => {
+
+    const [isCartVisible, setIsCartVisible] = useState(true);
+
+    const cartHandler = () => {
+        setIsCartVisible(!isCartVisible);
+    }
+
     return (
-        <Container fluid={true} className="m-0 p-0 bottom-header" >
+        <Navbar sticky='top' bg="white" expand="lg" className="bottom-header"
+                onClick={() => cartHandler()}
+        >
             <div className="shadow-line" ></div>
-            <Navbar bg="white" expand="lg">
-                <Navbar.Brand href="#home" className="text-success bottom-header-title" >
-                    <span>LOGO</span>
-                </Navbar.Brand>
+            <Navbar.Brand href="#home" className="text-success bottom-header-title" >
+                <span>LOGO</span>
+            </Navbar.Brand>
 
-                <Nav className="ml-auto">
-                    <Nav.Link href="#home" className="notification">
-                        <span><i className="feather icon-shopping-cart"/></span>
-                        <span className="badge">2</span>
-                    </Nav.Link>
-
-                </Nav>
-                <Nav.Link href="#link">
-                    <Button variant="success" size="lg" className="check-out-button" >Check out</Button>
+            <Nav className="ml-auto">
+                <Nav.Link href="#home" className="notification">
+                    <span><i className="feather icon-shopping-cart"/></span>
+                    <span className="badge">2</span>
+                    {isCartVisible && <CartArea/>}
                 </Nav.Link>
-            </Navbar>
-        </Container>
+
+            </Nav>
+            <Nav.Link href="#link">
+                <Button variant="success" size="sm" className="check-out-button" >Check out</Button>
+            </Nav.Link>
+        </Navbar>
     )
 }
 
---
+++ shopping-cart/src/components/homePage/headerArea/CartArea.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const CartArea:React.FC = () => {
+    return(
+        <div className="cart-area">Cart Area</div>
+    )
+}
+
+export default CartArea;
