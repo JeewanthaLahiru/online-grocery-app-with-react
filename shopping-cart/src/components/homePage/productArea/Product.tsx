b6a0239d0343bacda33af8ca8b9ab8d0404c0743
JeewanthaLahiru <jeewanthalahiru00@gmail.com>
2021-03-13 19:56:51 +0530

    created rendering functions according to categories

--- shopping-cart/src/components/homePage/productArea/Product.tsx
+++ shopping-cart/src/components/homePage/productArea/Product.tsx
@@ -1,22 +1,28 @@
 import React from 'react';
 import {Button, Col, Form, Image, Row} from "react-bootstrap";
 import Coconut from "../../../assets/images/products/coconut.jpg";
+import {IProduct} from "../../../types/Products";
 
-const Product:React.FC = () => {
+type ProductTypeProps = {
+    index: number,
+    product: IProduct
+}
+
+const Product:React.FC<ProductTypeProps> = (props) => {
     return(
         <Col xs={3} className="px-4 py-2">
             <Row className="product-class" >
                 <Col xs={12} className="image-container pt-3" >
-                    <Image src={Coconut} alt={"coconut"} />
+                    <Image src={props.product.image} alt={"coconut"} />
                 </Col>
                 <Col xs={12} >
-                    <h5 className="text-center mb-5" >Coconut</h5>
+                    <h5 className="text-center mb-5" >{props.product.name}</h5>
                 </Col>
                 <Col xs={6} >
-                    <p className="text-center discount" >50.00</p>
+                    <p className="text-center discount" >{props.product.price}.00</p>
                 </Col>
                 <Col xs={6} >
-                    <p className="text-center price text-success" >Rs.50.<span className="small-text">00</span></p>
+                    <p className="text-center price text-success" >Rs.{props.product.price}.<span className="small-text">00</span></p>
                 </Col>
 
                 <Col xs={5} className="cart-quantity" >
--- shopping-cart/src/components/homePage/productArea/ProductList.tsx
+++ shopping-cart/src/components/homePage/productArea/ProductList.tsx
@@ -1,19 +1,102 @@
 import React from "react";
-import {Row} from "react-bootstrap";
+import {Col, Row} from "react-bootstrap";
 import Product from "./Product";
+import {products} from "../../../repository/Products";
+import {IProduct} from "../../../types/Products";
 
 const ProductList:React.FC = () => {
+
+    const renderAllProducts = (category:string) => {
+        return(
+            products.map((product:IProduct, index:number)=>{
+                if(product.category === category){
+                    return(
+                        <Product product={product} index={index} />
+                    )
+                }
+            })
+        )
+    }
+    const renderGroceryList = () => {
+        return(
+            <React.Fragment>
+                <Row>
+                    <Col className="mt-4 ml-3">
+                        <h5>Grocery</h5>
+                    </Col>
+                </Row>
+                <Row className="m-0 p-0" >
+                    {renderAllProducts('Grocery')}
+                </Row>
+            </React.Fragment>
+        )
+    }
+    const renderPharmacyList = () => {
+        return(
+            <React.Fragment>
+                <Row>
+                    <Col className="mt-4 ml-3">
+                        <h5>Pharmacy</h5>
+                    </Col>
+                </Row>
+                <Row className="m-0 p-0" >
+                    {renderAllProducts('Pharmacy')}
+                </Row>
+            </React.Fragment>
+        )
+    }
+    const renderFoodList = () => {
+        return(
+            <React.Fragment>
+                <Row>
+                    <Col className="mt-4 ml-3">
+                        <h5>Food</h5>
+                    </Col>
+                </Row>
+                <Row className="m-0 p-0" >
+                    {renderAllProducts('Food')}
+                </Row>
+            </React.Fragment>
+        )
+    }
+    const renderElectronicList = () => {
+        return(
+            <React.Fragment>
+                <Row>
+                    <Col className="mt-4 ml-3">
+                        <h5>Electronic</h5>
+                    </Col>
+                </Row>
+                <Row className="m-0 p-0" >
+                    {renderAllProducts('Electronic')}
+                </Row>
+            </React.Fragment>
+        )
+    }
+    const renderAllCategory = () => {
+        return(
+            <React.Fragment>
+                <Row className="product-list mb-5">
+                    {renderGroceryList()}
+                </Row>
+                <Row className="product-list mb-5">
+                    {renderPharmacyList()}
+                </Row>
+                <Row className="product-list mb-5">
+                    {renderFoodList()}
+                </Row>
+                <Row className="product-list mb-5">
+                    {renderElectronicList()}
+                </Row>
+            </React.Fragment>
+        );
+    }
+
+
+
     return(
         <Row className="product-list p-4" >
-            <Product/>
-            <Product/>
-            <Product/>
-            <Product/>
-            <Product/>
-            <Product/>
-            <Product/>
-            <Product/>
-
+            {renderAllProducts("Grocery")}
         </Row>
     )
 }
