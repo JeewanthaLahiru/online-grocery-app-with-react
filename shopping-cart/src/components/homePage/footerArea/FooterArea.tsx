e3e7a5ffda42bb0428c0a725ea06bdeff7d76434
JeewanthaLahiru <jeewanthalahiru00@gmail.com>
2021-03-13 23:04:51 +0530

    created footer social media bar and imported to footer area

--- shopping-cart/src/components/homePage/footerArea/FooterArea.tsx
+++ shopping-cart/src/components/homePage/footerArea/FooterArea.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import {Col, Row} from "react-bootstrap";
 import FooterNavigationBar from "./FooterNavigationBar";
+import FooterSocialMediaBar from "./FooterSocialMediaBar";
 
 const FooterArea:React.FC = () => {
     return(
         <Row>
             <Col xs={{span:8, offset:2}} className="p-0 p-0" >
                 <FooterNavigationBar/>
+                <FooterSocialMediaBar/>
             </Col>
         </Row>
     )
---
+++ shopping-cart/src/components/homePage/footerArea/FooterSocialMediaBar.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const FooterSocialMediaBar:React.FC = () =>{
+    return(
+        <div>Footer navigation bar</div>
+    )
+}
+
+export default FooterSocialMediaBar;
