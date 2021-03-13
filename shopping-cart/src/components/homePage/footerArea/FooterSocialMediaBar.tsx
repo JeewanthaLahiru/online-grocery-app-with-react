f537aaf593072d138dd78f10298486f01d8b1e3b
JeewanthaLahiru <jeewanthalahiru00@gmail.com>
2021-03-13 23:22:46 +0530

    created footer socialmedia bar

--- shopping-cart/src/components/homePage/footerArea/FooterSocialMediaBar.tsx
+++ shopping-cart/src/components/homePage/footerArea/FooterSocialMediaBar.tsx
@@ -1,8 +1,22 @@
 import React from 'react';
 
+import {Image} from "react-bootstrap";
+import Facebook from '../../../assets/images/icons/facebook.svg';
+import Linkedin from '../../../assets/images/icons/linkedin.svg';
+import Rss from '../../../assets/images/icons/rss.svg';
+import Twitter from '../../../assets/images/icons/twitter.svg';
+import Youtube from '../../../assets/images/icons/youtube.svg';
+
+
 const FooterSocialMediaBar:React.FC = () =>{
     return(
-        <div>Footer navigation bar</div>
+        <div className ='text-center py-md-2'>
+            <Image className='social-icon px-1' src={Facebook} alt={'logo1'}/>
+            <Image className='social-icon px-1' src={Linkedin} alt={'logo2'}/>
+            <Image className='social-icon px-1' src={Rss} alt={'logo3'}/>
+            <Image className='social-icon px-1' src={Twitter} alt={'logo4'}/>
+            <Image className='social-icon px-1' src={Youtube} alt={'logo5'}/>
+        </div>
     )
 }
 
