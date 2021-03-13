ebd0b1e57ebac8680eabb2c77b0e4e105580f35f
JeewanthaLahiru <jeewanthalahiru00@gmail.com>
2021-03-13 20:28:38 +0530

    changed category name

--- shopping-cart/src/components/homePage/productArea/ProductList.tsx
+++ shopping-cart/src/components/homePage/productArea/ProductList.tsx
@@ -50,7 +50,7 @@ const ProductList:React.FC = () => {
             <Col xs={12} className="m-0 p-0">
                 <Row>
                     <Col className="mt-4 ml-3">
-                        <h5 className="text-left">Food</h5>
+                        <h5 className="text-left">Foods</h5>
                     </Col>
                 </Row>
                 <Row className="m-0 p-0" >
