1fa7ff6e1f03b5bdd74bb45b8db0a485ac25dad7
JeewanthaLahiru <jeewanthalahiru00@gmail.com>
2021-03-14 17:22:20 +0530

    created cart items to show

--- shopping-cart/src/components/homePage/headerArea/CartProduct.tsx
+++ shopping-cart/src/components/homePage/headerArea/CartProduct.tsx
@@ -1,8 +1,39 @@
 import React from "react";
+import {ICartProduct} from "../../../types/CartProducts";
+import {Col, Row, Image} from "react-bootstrap";
 
-const CartProduct:React.FC = () => {
+type CartProductProps = {
+    cartProduct: ICartProduct,
+}
+
+const CartProduct:React.FC<CartProductProps> = (props) => {
+    const {cartProduct} = props;
+
     return(
-        <div>cart product</div>
+        <Row className={'cart-product'}>
+            <Col xs={4} className={'pr-0'}>
+                <Image className='cart-product-img' src={cartProduct.product.image} rounded={true}/>
+            </Col>
+            <Col xs={8}>
+                <Row>
+                    <Col xs={10} className={'mb-1 mb-sm-2 cart-product-title'}>
+                        {cartProduct.product.name}
+                    </Col>
+                    <Col xs={2} className={'mb-1 mb-sm-2 text-center p-0 pr-2 m-0'}>
+                        <i className="feather icon-trash" />
+                    </Col>
+                </Row>
+                <Row className='mt-1 mt-sm-2'>
+                    <Col xs={4} className='cart-product-qty'>
+                        Qty: {cartProduct.qty}
+                    </Col>
+                    <Col xs={8} className='cart-product-price text-right'>
+                        Rs. {cartProduct.product.price*cartProduct.qty}.00
+                    </Col>
+                </Row>
+            </Col>
+            <hr/>
+        </Row>
     )
 }
 
--- shopping-cart/src/components/homePage/headerArea/CartProductList.tsx
+++ shopping-cart/src/components/homePage/headerArea/CartProductList.tsx
@@ -1,13 +1,27 @@
 import React from 'react';
 import {ICartProduct} from "../../../types/CartProducts";
+import {Container} from "react-bootstrap";
+import CartProduct from "./CartProduct";
 
 type cartProductListProps = {
     cartProducts: ICartProduct[]
 }
 
 const CartProductList:React.FC<cartProductListProps> = (props) => {
+
+    const {cartProducts} = props;
+
     return(
-        <div>hello</div>
+        <Container className='cart-product-list'>
+            {cartProducts.map((cartProduct: ICartProduct, index: number) => {
+                return (
+                    <React.Fragment key={index}>
+                        <CartProduct cartProduct={cartProduct}/>
+                        <hr/>
+                    </React.Fragment>
+                )
+            })}
+        </Container>
     )
 }
 
