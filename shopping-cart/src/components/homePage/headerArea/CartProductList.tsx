aa5b7d5e3ca317579c1b7aa8082a8df0c052601a
JeewanthaLahiru <jeewanthalahiru00@gmail.com>
2021-03-14 19:16:13 +0530

    added empty cart image to cart

--- shopping-cart/src/components/homePage/headerArea/CartProductList.tsx
+++ shopping-cart/src/components/homePage/headerArea/CartProductList.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import {ICartProduct} from "../../../types/CartProducts";
-import {Container} from "react-bootstrap";
+import {Button, Col, Container, Row, Image} from "react-bootstrap";
 import CartProduct from "./CartProduct";
 import CartBillingArea from "./CartBillingArea";
+import EmptyImg from '../../../assets/images/ui/emptycheckout.png';
 
 type cartProductListProps = {
     cartProducts: ICartProduct[]
@@ -12,19 +13,38 @@ const CartProductList:React.FC<cartProductListProps> = (props) => {
 
     const {cartProducts} = props;
 
+    const renderCartProductList = () => {
+        if (cartProducts.length === 0) {
+            return (
+                <Container >
+                    <Image src={EmptyImg} className='p-0' fluid={true}/>
+                    <Row className='justify-content-center mb-4'>
+                        <Col className='text-center'>
+                            <Button variant='outline-success'>Let's Shopping</Button>
+                        </Col>
+                    </Row>
+                </Container>
+            )
+        } else {
+            return (
+                <Container className='cart-product-list'>
+                    {cartProducts.map((cartProduct: ICartProduct, index: number) => {
+                        return (
+                            <React.Fragment key={index}>
+                                <CartProduct cartProduct={cartProduct}/>
+                                <hr/>
+                            </React.Fragment>
+                        )
+                    })}
+                </Container>
+            )
+        }
+    }
+
     return(
         <React.Fragment>
-            <Container className='cart-product-list'>
-                {cartProducts.map((cartProduct: ICartProduct, index: number) => {
-                    return (
-                        <React.Fragment key={index}>
-                            <CartProduct cartProduct={cartProduct}/>
-                            <hr/>
-                        </React.Fragment>
-                    )
-                })}
-            </Container>
-            <CartBillingArea/>
+            {renderCartProductList()}
+            { cartProducts.length !== 0 && <CartBillingArea/>}
         </React.Fragment>
     )
 }
