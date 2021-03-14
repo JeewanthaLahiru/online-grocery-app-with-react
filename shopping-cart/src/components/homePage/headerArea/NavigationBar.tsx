f418e65b67c6249d308a31128db80e1c066466cd
JeewanthaLahiru <jeewanthalahiru00@gmail.com>
2021-03-15 00:05:08 +0530

    change category state with dropdown

--- shopping-cart/src/components/homePage/headerArea/NavigationBar.tsx
+++ shopping-cart/src/components/homePage/headerArea/NavigationBar.tsx
@@ -1,16 +1,29 @@
 import React from "react";
 import {Container, DropdownButton, Navbar, Nav, Dropdown} from "react-bootstrap";
+import {useDispatch} from "react-redux";
+import {changeCategory} from "../../../store/actions/CategoryActions";
 
 const NavigationBar:React.FC = () => {
+
+    const dispatch = useDispatch();
+
     return (
         <Container fluid={true} className="navBar">
             <Navbar bg="white" >
 
                 <DropdownButton title="Categories" variant="outline-secondary">
-                    <Dropdown.Item className='dropdown-item' href="#/action-1">Grocery</Dropdown.Item>
-                    <Dropdown.Item className='dropdown-item' href="#/action-2">Pharmacy</Dropdown.Item>
-                    <Dropdown.Item className='dropdown-item' href="#/action-3">Food</Dropdown.Item>
-                    <Dropdown.Item className='dropdown-item' href="#/action-3">Electronic</Dropdown.Item>
+                    <Dropdown.Item className='dropdown-item' href="#/action-1"
+                        onClick={()=>dispatch(changeCategory('Grocery'))}
+                    >Grocery</Dropdown.Item>
+                    <Dropdown.Item className='dropdown-item' href="#/action-2"
+                                   onClick={()=>dispatch(changeCategory('Pharmacy'))}
+                    >Pharmacy</Dropdown.Item>
+                    <Dropdown.Item className='dropdown-item' href="#/action-3"
+                                   onClick={()=>dispatch(changeCategory('Food'))}
+                    >Food</Dropdown.Item>
+                    <Dropdown.Item className='dropdown-item' href="#/action-3"
+                                   onClick={()=>dispatch(changeCategory('Electronic'))}
+                    >Electronic</Dropdown.Item>
                 </DropdownButton>
                 <Nav.Link href="#home" className="text-secondary" >Home</Nav.Link>
                 <Nav.Link href="#link" className="text-secondary" >FAQ</Nav.Link>
