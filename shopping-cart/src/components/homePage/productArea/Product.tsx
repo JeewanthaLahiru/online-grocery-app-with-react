2cf16700e157beded310fb09abbfb15c0bcdbb90
JeewanthaLahiru <jeewanthalahiru00@gmail.com>
2021-03-14 19:50:41 +0530

    take product values via props

--- shopping-cart/src/components/homePage/productArea/Product.tsx
+++ shopping-cart/src/components/homePage/productArea/Product.tsx
@@ -78,13 +78,13 @@ const Product:React.FC<ProductTypeProps> = (props) => {
         <Col xs={6} md={4} lg={3} className="px-4 py-2">
             <Row className="product-class" >
                 <Col xs={12} className="image-container pt-3" >
-                    <Image src={props.product.image} alt={"coconut"} />
+                    <Image src={product_image} alt={"coconut"} />
                 </Col>
                 <Col xs={12} >
-                    <h5 className="text-center mb-5" >{props.product.name}</h5>
+                    <h5 className="text-center mb-5" >{product_name}</h5>
                 </Col>
                 <Col xs={6} >
-                    <p className="text-center discount" >{props.product.price}.00</p>
+                    <p className="text-center discount" >{product_price}.00</p>
                 </Col>
                 <Col xs={6} >
                     <p className="text-center price text-success" >Rs.{props.product.price}.<span className="small-text">00</span></p>
