aef5ffcdb32a50b1db8b23a671a7470a0014565f
JeewanthaLahiru <jeewanthalahiru00@gmail.com>
2021-06-13 06:22:21 +0530

    created register form types

--- shopping-cart/src/components/UserPages/Register/Register.tsx
+++ shopping-cart/src/components/UserPages/Register/Register.tsx
@@ -1,9 +1,33 @@
 import React from 'react';
+import {Col, Form, Row} from "react-bootstrap";
+import {Controller, useForm} from "react-hook-form";
 
 const Register:React.FC = () => {
     return(
         <React.Fragment>
-            Hello register
+            <Row className="mx-0 justify-content-center register-form-root">
+                <Col lg={6} className="register-form my-4" >
+                    <Row className="mx-0 align-items-center " >
+                        <Col xs={4}>
+                            <hr/>
+                        </Col>
+                        <Col xs={4}>
+                            <h1>Register</h1>
+                        </Col>
+                        <Col xs={4}>
+                            <hr/>
+                        </Col>
+                    </Row>
+                    <Form>
+                        <Row className="mx-0" >
+                            <Col xs={12}>
+
+                            </Col>
+                            <Col xs={12}></Col>
+                        </Row>
+                    </Form>
+                </Col>
+            </Row>
         </React.Fragment>
     )
 }
