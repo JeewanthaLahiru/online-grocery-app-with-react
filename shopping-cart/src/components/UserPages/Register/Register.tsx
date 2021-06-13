3ec710a9674a893bb55cc85036d28824573bed51
JeewanthaLahiru <jeewanthalahiru00@gmail.com>
2021-06-13 07:10:28 +0530

    added validations

--- shopping-cart/src/components/UserPages/Register/Register.tsx
+++ shopping-cart/src/components/UserPages/Register/Register.tsx
@@ -9,13 +9,16 @@ const Register:React.FC = () => {
     const onCheckBoxClick = () => {
         setCheckbox(!checkBox);
     }
+    const handleOnSubmit = () => {
+        console.log('hello');
+    }
 
     const {handleSubmit, control, errors, reset, setValue} = useForm<registerTypes>();
 
     return(
         <React.Fragment>
             <Row className="mx-0 justify-content-center register-form-root">
-                <Col lg={4} className="register-form my-4" >
+                <Col lg={4} className="shadow register-form my-4" >
                     <Row className="mx-0 align-items-center " >
                         <Col xs={4}>
                             <hr/>
@@ -27,29 +30,31 @@ const Register:React.FC = () => {
                             <hr/>
                         </Col>
                     </Row>
-                    <Form>
+                    <Form onSubmit={handleSubmit(handleOnSubmit)} >
                         <Row className="mx-0" >
                             <Col xs={6}>
                                 <Controller
                                     control={control}
-                                    name={"first-name"}
+                                    name={"firstName"}
                                     as={<Form.Control placeholder={"First Name"} className='registerInput'/>}
                                     defaultValue={''}
                                     rules={{
                                         required: true
                                     }}
                                 />
+                                {errors.firstName && <span className="text-danger">* This field is required</span>}
                             </Col>
                             <Col xs={6}>
                                 <Controller
                                     control={control}
-                                    name={"last-name"}
+                                    name={"lastName"}
                                     as={<Form.Control placeholder={"Last Name"} className="registerInput"/>}
                                     defaultValue={''}
                                     rules={{
                                         required : true
                                     }}
                                 />
+                                {errors.lastName && <span className="text-danger">* This field is required</span>}
                             </Col>
                         </Row>
                         <Row className={"mx-0"} >
@@ -64,6 +69,7 @@ const Register:React.FC = () => {
                                     }}
                                 />
                             </Col>
+                            {errors.email && <span className="text-danger ml-4">* This field is required</span>}
                         </Row>
                         <Row className={"mx-0"} >
                             <Col xs={12}>
@@ -77,6 +83,7 @@ const Register:React.FC = () => {
                                     }}
                                 />
                             </Col>
+                            {errors.password && <span className="text-danger ml-4">* This field is required</span>}
                         </Row>
                         <Row className={"mx-0"} >
                             <Col xs={12}>
@@ -90,6 +97,7 @@ const Register:React.FC = () => {
                                     }}
                                 />
                             </Col>
+                            {errors.confirmPassword && <span className="text-danger ml-4">* This field is required</span>}
                         </Row>
                         <Row className={"mx-0"}>
                             <Col xs={12}>
@@ -102,7 +110,7 @@ const Register:React.FC = () => {
                         </Row>
                         <Row className="mx-0" >
                             <Col xs={12}>
-                                <Button variant="success" disabled={!checkBox}>
+                                <Button variant="success" disabled={!checkBox} type={"submit"}>
                                     Register
                                 </Button>
                             </Col>
