2610d039675a66cfb8e47225af6807cea40b6e6b
JeewanthaLahiru <jeewanthalahiru00@gmail.com>
2021-06-13 06:45:20 +0530

    added input fields

--- shopping-cart/src/components/UserPages/Register/Register.tsx
+++ shopping-cart/src/components/UserPages/Register/Register.tsx
@@ -1,8 +1,17 @@
-import React from 'react';
-import {Col, Form, Row} from "react-bootstrap";
+import React, {useState} from 'react';
+import {Button, Col, Form, Row} from "react-bootstrap";
 import {Controller, useForm} from "react-hook-form";
+import {registerTypes} from "../../../types/RegisterTypes";
 
 const Register:React.FC = () => {
+    const [checkBox, setCheckbox] = useState(false);
+
+    const onCheckBoxClick = () => {
+        setCheckbox(!checkBox);
+    }
+
+    const {handleSubmit, control, errors, reset, setValue} = useForm<registerTypes>();
+
     return(
         <React.Fragment>
             <Row className="mx-0 justify-content-center register-form-root">
@@ -20,10 +29,83 @@ const Register:React.FC = () => {
                     </Row>
                     <Form>
                         <Row className="mx-0" >
-                            <Col xs={12}>
-
+                            <Col xs={6}>
+                                <Controller
+                                    control={control}
+                                    name={"first-name"}
+                                    as={<Form.Control placeholder={"First Name"} className='registerInput'/>}
+                                    defaultValue={''}
+                                    rules={{
+                                        required: true
+                                    }}
+                                />
                             </Col>
-                            <Col xs={12}></Col>
+                            <Col xs={6}>
+                                <Controller
+                                    control={control}
+                                    name={"last-name"}
+                                    as={<Form.Control placeholder={"Last Name"} className="registerInput"/>}
+                                    defaultValue={''}
+                                    rules={{
+                                        required : true
+                                    }}
+                                />
+                            </Col>
+                        </Row>
+                        <Row className={"mx-0"} >
+                            <Col xs={12}>
+                                <Controller
+                                    control={control}
+                                    name={"email"}
+                                    as={<Form.Control placeholder={"Email"} className="registerInput" />}
+                                    defaultValue={""}
+                                    rules={{
+                                        required: true
+                                    }}
+                                />
+                            </Col>
+                        </Row>
+                        <Row className={"mx-0"} >
+                            <Col xs={12}>
+                                <Controller
+                                    control={control}
+                                    name={"password"}
+                                    as={<Form.Control placeholder={"Password"} className="registerInput" />}
+                                    defaultValue={""}
+                                    rules={{
+                                        required: true
+                                    }}
+                                />
+                            </Col>
+                        </Row>
+                        <Row className={"mx-0"} >
+                            <Col xs={12}>
+                                <Controller
+                                    control={control}
+                                    name={"confirmPassword"}
+                                    as={<Form.Control placeholder={"Confirm password"} className="registerInput" />}
+                                    defaultValue={""}
+                                    rules={{
+                                        required: true
+                                    }}
+                                />
+                            </Col>
+                        </Row>
+                        <Row className={"mx-0"}>
+                            <Col xs={12}>
+                                <Form.Check
+                                    type="checkbox"
+                                    label="I accept Terms of Use and Privacy Policy"
+                                    onChange={onCheckBoxClick}
+                                />
+                            </Col>
+                        </Row>
+                        <Row className="mx-0" >
+                            <Col xs={12}>
+                                <Button variant="success" disabled={!checkBox}>
+                                    Register
+                                </Button>
+                            </Col>
                         </Row>
                     </Form>
                 </Col>
