222b89d11ed62116538741fa5779857ee344db6c
JeewanthaLahiru <jeewanthalahiru00@gmail.com>
2021-06-13 09:21:53 +0530

    create login type

--- shopping-cart/src/components/UserPages/login/Login.tsx
+++ shopping-cart/src/components/UserPages/login/Login.tsx
@@ -1,9 +1,23 @@
 import React from 'react';
+import {Col, Image, Row} from "react-bootstrap";
+import CartImg from "../../../assets/images/login/cart.png";
+import { Controller, useForm } from 'react-hook-form';
 
 const Login:React.FC = () => {
     return(
         <React.Fragment>
-            hello login
+            <Row className="mx-0 my-4 justify-content-center">
+                <Col xs={8} className="login-page" >
+                    <Row className="mx-0 align-items-center" >
+                        <Col xs={6}>
+                            <Image src={CartImg}/>
+                        </Col>
+                        <Col xs={6}>
+
+                        </Col>
+                    </Row>
+                </Col>
+            </Row>
         </React.Fragment>
     )
 }
