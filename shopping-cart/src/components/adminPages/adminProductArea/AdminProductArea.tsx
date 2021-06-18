6ee40df63e2514e0b23215073a0010b7858594ca
JeewanthaLahiru <jeewanthalahiru00@gmail.com>
2021-06-18 11:40:22 +0530

    created admin product header cover image

--- shopping-cart/src/components/adminPages/adminProductArea/AdminProductArea.tsx
+++ shopping-cart/src/components/adminPages/adminProductArea/AdminProductArea.tsx
@@ -1,9 +1,19 @@
 import React from "react";
+import {Col, Row, Image} from "react-bootstrap";
+import CoverIMG from "../../../assets/images/banner_images/banner.jpg";
 
 const AdminProductArea:React.FC = () => {
     return(
         <React.Fragment>
-            hello this is adminProductArea page
+            <Row className="admin-product-row justify-content-center mx-0 mt-3">
+                <Col xs={8} className="admin-product-body">
+                    <Row className="mx-0">
+                        <Col xs={12} className="header-area">
+                            <Image src={CoverIMG} />
+                        </Col>
+                    </Row>
+                </Col>
+            </Row>
         </React.Fragment>
     )
 }
