b60b2ceb42c6f243ea6c2ebd7baa8be61ea23c5d
JeewanthaLahiru <jeewanthalahiru00@gmail.com>
2021-06-18 12:54:17 +0530

    added table details on admin products

--- shopping-cart/src/components/adminPages/adminProductArea/ProductTable.tsx
+++ shopping-cart/src/components/adminPages/adminProductArea/ProductTable.tsx
@@ -1,45 +1,87 @@
 import React from "react";
-import {Col} from "react-bootstrap";
+import {Button, Col} from "react-bootstrap";
 import BootstrapTable from "react-bootstrap-table-next";
+import { products } from "../../../repository/Products";
+import {IProduct} from "../../../types/Products";
 
 const ProductTable:React.FC = () => {
 
-    const columns = [{
-        dataField: 'id',
-        text: 'Product ID',
-        sort: true
-    }, {
-        dataField: 'name',
-        text: 'Product Name',
-        sort: true
-    }, {
-        dataField: 'price',
-        text: 'Product Price',
-        sort: true
-    }];
-
-    const sampleData = [
+    const columns = [
         {
-            id:"sampleid001",
-            name: "samplename001",
-            price: "sampleprice001",
-            prevP: "sapmleprevprice001"
+            dataField: "id",
+            text:"#",
+            sort: true
         },
         {
-            id:"sampleid002",
-            name: "samplename002",
-            price: "sampleprice002",
-            prevP: "samplePrevprice002"
+            dataField: "item",
+            text:"Item",
+            sort: true
+        },
+        {
+            dataField: "name",
+            text: "Name",
+            sort: true
+        },
+        {
+            dataField: "description",
+            text: "Descritption",
+            sort: true
+        },
+        {
+            dataField: "prevprice",
+            text: "Prev. Price",
+            sort: true
+        },
+        {
+            dataField: "price",
+            text: "Price",
+            sort: true
+        },
+        {
+            dataField: "edit",
+            text: ""
+        },
+        {
+            dataField: "delete",
+            text: ""
         }
-    ]
+        ];
+
+    const productGenerator = (products:IProduct[]): any[] => {
+        const checkList : any[] = [];
+
+        products.map((product, i: number) => {
+            checkList.push({
+                id: i+1,
+                item: product.category,
+                name: product.name,
+                description: product.name,
+                prevprice: product.price,
+                price: product.price,
+                edit:
+                    <React.Fragment>
+                        <Button variant={"secondary"}><i className="feather icon-edit" ></i></Button>
+                    </React.Fragment>,
+                delete:
+                    <React.Fragment>
+                        <Button variant={"danger"}><i className="feather icon-delete" ></i></Button>
+                    </React.Fragment>,
+            })
+        })
+
+        return checkList;
+    }
+
 
     return(
         <React.Fragment>
             <Col xs={12}>
                 <BootstrapTable
-                    keyField="id"
-                    data={sampleData}
-                    columns={columns}/>
+                    keyField={"id"}
+                    data={productGenerator(products)}
+                    columns={columns}
+                    bordered={false}
+                />
             </Col>
         </React.Fragment>
     )
