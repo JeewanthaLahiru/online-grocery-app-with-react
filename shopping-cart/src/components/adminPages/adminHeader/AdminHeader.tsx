a62cd5074156dccbeca95b51ecd920e732f1ee6e
JeewanthaLahiru <jeewanthalahiru00@gmail.com>
2021-06-18 07:54:06 +0530

    created admin header, admin profile components

---
+++ shopping-cart/src/components/adminPages/adminHeader/AdminHeader.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const AdminHeader:React.FC = () => {
+    return(
+        <React.Fragment>
+            Hello
+        </React.Fragment>
+    )
+}
+
+export default AdminHeader;
---
+++ shopping-cart/src/components/adminPages/adminProfile/AdminProfile.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const AdminProfile:React.FC = () => {
+    return(
+        <React.Fragment>
+            profile
+        </React.Fragment>
+    )
+}
+
+export default AdminProfile;
