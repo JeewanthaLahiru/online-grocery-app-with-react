0cc66db5689befa16221eb1d2f3bdd84fde19496
JeewanthaLahiru <jeewanthalahiru00@gmail.com>
2021-06-19 15:51:51 +0530

    create admin order component

---
+++ shopping-cart/src/components/adminPages/adminOrders/AdminOrder.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const AdminOrder:React.FC = () => {
+    return(
+        <React.Fragment>
+            admin order
+        </React.Fragment>
+    )
+}
+
+export default AdminOrder;
