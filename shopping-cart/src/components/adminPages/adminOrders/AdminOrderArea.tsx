9e2c3ce0bb72af679b300798bb03f605f22c3f8c
JeewanthaLahiru <jeewanthalahiru00@gmail.com>
2021-06-19 10:34:30 +0530

    created navButtons

--- shopping-cart/src/components/adminPages/adminOrders/AdminOrderArea.tsx
+++ shopping-cart/src/components/adminPages/adminOrders/AdminOrderArea.tsx
@@ -1,9 +1,51 @@
-import React from "react";
+import React, {useState} from "react";
+import {Col, Form, Image, Row} from "react-bootstrap";
+import CoverImg from "../../../assets/images/banner_images/banner.jpg";
 
 const AdminOrderArea:React.FC = () => {
+
+    const[isPending, setIsPending] = useState(true);
+    const handleOnPendingClick = () => {
+        setIsPending(true);
+        console.log(isPending);
+    }
+    const handleOnAcceptClick = () => {
+        setIsPending(false);
+    }
+
     return(
         <React.Fragment>
-            hello adminOrderArea
+            <Row className="mx-0 admin-order-row justify-content-center my-3">
+                <Col xs={12} md={10} xl={8} className="admin-order-body" >
+                    <Row className="mx-0 nav-area">
+                        <Col xs={12} >
+                            <ul>
+                                <li>
+                                    <Form.Label
+                                        className={isPending?"text-bold":""}
+                                        onClick={handleOnPendingClick}
+                                    >
+                                        Pending orders
+                                    </Form.Label>
+                                </li>
+                                <li>
+                                    <Form.Label
+                                        className={!isPending?"text-bold":""}
+                                        onClick={handleOnAcceptClick}
+                                    >
+                                        Accepted orders
+                                    </Form.Label>
+                                </li>
+                            </ul>
+                        </Col>
+                    </Row>
+                    <Row className="mx-0 header-area ">
+                        <Col xs={12}>
+                            <Image src={CoverImg} />
+                        </Col>
+                    </Row>
+                </Col>
+            </Row>
         </React.Fragment>
     )
 }
