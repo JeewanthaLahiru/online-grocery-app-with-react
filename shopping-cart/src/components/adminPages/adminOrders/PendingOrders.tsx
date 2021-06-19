2f99f02a387790240fe7ad0b2e3a4681f3bf30bd
JeewanthaLahiru <jeewanthalahiru00@gmail.com>
2021-06-19 10:36:49 +0530

    created pendingorders component

---
+++ shopping-cart/src/components/adminPages/adminOrders/PendingOrders.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const PendingOrders = () => {
+    return(
+        <React.Fragment>
+            Pending orders
+        </React.Fragment>
+    )
+}
+
+export default PendingOrders;
