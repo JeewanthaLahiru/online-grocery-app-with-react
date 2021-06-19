8b3f8e0421f8523f3d0556bd4414606071ec53b2
JeewanthaLahiru <jeewanthalahiru00@gmail.com>
2021-06-19 09:32:03 +0530

    created admin order area component

---
+++ shopping-cart/src/components/adminPages/adminOrders/AdminOrderArea.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const AdminOrderArea:React.FC = () => {
+    return(
+        <React.Fragment>
+            hello adminOrderArea
+        </React.Fragment>
+    )
+}
+
+export default AdminOrderArea;
