e7f87115912667a0f1512f0ec31089a83c89f43a
JeewanthaLahiru <jeewanthalahiru00@gmail.com>
2021-06-21 12:55:32 +0530

    added more attributes to product interface

--- shopping-cart/src/types/Products.ts
+++ shopping-cart/src/types/Products.ts
@@ -1,7 +1,9 @@
 export interface IProduct{
-    id:number
-    name:string
-    category:string
-    price:number
-    image:string
+    id: number
+    name: string
+    category: string
+    price: number
+    previousPrice: number
+    image: string
+    description: string
 }
