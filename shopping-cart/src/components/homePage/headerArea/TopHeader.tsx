648701d4be57427f4cb215ba3a3040f7a9e94ea9
JeewanthaLahiru <jeewanthalahiru00@gmail.com>
2021-06-21 15:24:20 +0530

    login logout function with redux

--- shopping-cart/src/components/homePage/headerArea/TopHeader.tsx
+++ shopping-cart/src/components/homePage/headerArea/TopHeader.tsx
@@ -1,8 +1,18 @@
 import React from 'react';
 import {Button, Container, Nav, Navbar} from "react-bootstrap";
 import {Link} from 'react-router-dom';
+import {useDispatch, useSelector} from "react-redux";
+import {AppState} from "../../../store/reducers";
+import {logout} from "../../../store/actions/AuthActions";
 
 const TopHeader:React.FC = () => {
+    const loggedState = useSelector((state:AppState) => state.auth.logged);
+    const dispatch = useDispatch();
+
+    const handleOnLogOutClick = () => {
+        dispatch(logout(false));
+    }
+
     return (
         <Container fluid={true} className="top-header" >
             <Navbar bg="white" expand="lg">
@@ -11,16 +21,28 @@ const TopHeader:React.FC = () => {
                     <Nav className="ml-auto" >
                         <Nav.Link href="#home"> <i className="feather icon-truck"/> Delivery Areas</Nav.Link>
                         <Nav.Link href="#link"> <i className="feather icon-phone-call" /> +94 112 123 456</Nav.Link>
-                        <Nav.Link href="#home">
-                            <Link to={"/register"}>
-                                <Button variant="outline-success">Register</Button>
-                            </Link>
-                        </Nav.Link>
-                        <Nav.Link href="#home">
-                            <Link to={"/login"}>
-                                <Button variant="light" className="text-success" >Login</Button>
-                            </Link>
-                        </Nav.Link>
+                        {!loggedState &&
+                            <Nav.Link>
+                                <Link to={"/register"}>
+                                    <Button variant="outline-success">Register</Button>
+                                </Link>
+                            </Nav.Link>
+                        }
+                        {!loggedState &&
+                            <Nav.Link href="#home">
+                                <Link to={"/login"}>
+                                    <Button variant="light" className="text-success" >Login</Button>
+                                </Link>
+                            </Nav.Link>
+                        }
+                        {loggedState &&
+                            <Button
+                                variant="danger"
+                                onClick={handleOnLogOutClick}
+                            >
+                                Log out
+                            </Button>
+                        }
                     </Nav>
                 </Navbar.Collapse>
             </Navbar>
