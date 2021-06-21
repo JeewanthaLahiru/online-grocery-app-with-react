1acee538b68df101e1ff68f446736663aa503be8
JeewanthaLahiru <jeewanthalahiru00@gmail.com>
2021-06-21 18:11:10 +0530

    added profile picture area

--- shopping-cart/src/components/adminPages/adminProfile/AdminProfile.tsx
+++ shopping-cart/src/components/adminPages/adminProfile/AdminProfile.tsx
@@ -1,10 +1,22 @@
 import React from "react";
-import AdminHeader from "../adminHeader/AdminHeader";
+import Profile from "../../../assets/images/default/default.jpg";
+import {Col, Row, Image} from "react-bootstrap";
 
 const AdminProfile:React.FC = () => {
     return(
         <React.Fragment>
-            <AdminHeader/>
+            <Row className="mx-0 profile-page" >
+                <Col xs={4} className="profile-left">
+                    <Row className="mx-0 mt-5 profile-image justify-content-center">
+                        <Col xs={9}>
+                            <Image src={Profile} />
+                        </Col>
+                    </Row>
+                </Col>
+                <Col xs={8} className="profile-right">
+
+                </Col>
+            </Row>
         </React.Fragment>
     )
 }
