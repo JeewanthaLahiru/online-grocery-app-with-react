8d1adc516840ba516822eb735ff0d65e2fc49ac8
JeewanthaLahiru <jeewanthalahiru00@gmail.com>
2021-07-01 12:12:46 +0530

    added confirm and cancel buttons

--- shopping-cart/src/components/homePage/ConfirmationMessage.tsx
+++ shopping-cart/src/components/homePage/ConfirmationMessage.tsx
@@ -5,14 +5,15 @@ const ConfirmationMessage:React.FC = () => {
     return(
         <Row className="m-0 p-0">
             <Col xs={12}>
-                <Row className="m-0 p-0">
+                <Row className="m-0 p-0 border-success">
                     <Col xs={12} className="bg-success">
-                        hello world
+                        <h2 >Confirm Delete</h2>
                     </Col>
-                    <Col xs={12} className="bg-white">
-                        Confirm?
+                    <Col xs={12} className="bg-white py-4">
+                        <span >Are you sure want to delete this product?</span>
                     </Col>
-                    <Col xs={12}>
+                    <Col xs={12} className="p-0 ">
+                        <hr className="bg-success py-0 px-0 mx-0 mb-1" />
                         <Button className="float-right" variant="light" >Cancel</Button>
                         <Button className="float-right mx-1" variant="warning">Confirm </Button>
                     </Col>
