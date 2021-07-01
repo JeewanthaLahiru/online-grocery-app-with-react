3b360ebc27b40d510038fd36d7fca093cf798594
JeewanthaLahiru <jeewanthalahiru00@gmail.com>
2021-07-01 12:28:26 +0530

    added button colors to confirm

--- shopping-cart/src/components/homePage/ConfirmationMessage.tsx
+++ shopping-cart/src/components/homePage/ConfirmationMessage.tsx
@@ -3,19 +3,19 @@ import {Button, Col, Row} from "react-bootstrap";
 
 const ConfirmationMessage:React.FC = () => {
     return(
-        <Row className="m-0 p-0">
+        <Row className="m-0 p-0 confirm-message-area">
             <Col xs={12}>
                 <Row className="m-0 p-0 border-success">
-                    <Col xs={12} className="bg-success">
+                    <Col xs={12} className="bg-success first-row">
                         <h2 >Confirm Delete</h2>
                     </Col>
-                    <Col xs={12} className="bg-white py-4">
+                    <Col xs={12} className="bg-white py-4 second-row">
                         <span >Are you sure want to delete this product?</span>
                     </Col>
-                    <Col xs={12} className="p-0 ">
-                        <hr className="bg-success py-0 px-0 mx-0 mb-1" />
-                        <Button className="float-right" variant="light" >Cancel</Button>
-                        <Button className="float-right mx-1" variant="warning">Confirm </Button>
+                    <Col xs={12} className="pb-3 third-row">
+                        <hr/>
+                        <Button className="float-right" variant="outline-danger" >Cancel</Button>
+                        <Button className="float-right mx-1" variant="danger">Confirm </Button>
                     </Col>
                 </Row>
             </Col>
